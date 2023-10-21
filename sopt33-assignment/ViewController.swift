2b53531a9ae60e1bf93783f8e57f737ffb8fed88
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-10-22 01:10:51 +0900

    [Feat] #1 시작화면 리스트뷰 구현

--- sopt33-assignment/ViewController.swift
+++ sopt33-assignment/ViewController.swift
@@ -29,12 +29,12 @@ final class ViewController: UIViewController {
         return view
     }()
     
-    var optionImageView: UIImageView = {
+    private var optionImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "options"))
         return imageView
     }()
     
-    var titleLabel: UILabel = {
+    private var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "날씨"
         label.font = UIFont(name: "SFProText-Bold", size: 36)
@@ -42,16 +42,73 @@ final class ViewController: UIViewController {
         return label
     }()
     
-    var searchTextField: UITextField = {
+    private var searchTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "도시 또는 공항 검색"
         textField.font = UIFont(name: "SFProText-Medium", size: 19)
         textField.layer.cornerRadius = 10
         textField.backgroundColor = .systemBackground
-        textField.attributedPlaceholder = NSAttributedString(string: "도시 또는 공항 검색", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "도시 또는 공항 검색", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         textField.textColor = .lightText
         textField.setLeftPaddingPoints(58)
         return textField
+    }()
+    
+    private var weatherCardView: UIView = {
+        let cardView = UIView()
+        let weatherListImageView = UIImageView(image: UIImage(named: "weatherlist"))
+        weatherListImageView.frame = CGRect(x: 0, y: 0, width: 335, height: 117)
+        cardView.addSubview(weatherListImageView)
+        
+        var myLocationLabel = UILabel()
+        myLocationLabel.text = "나의 위치"
+        myLocationLabel.font =  UIFont(name: "SFProText-Bold", size: 24)
+        myLocationLabel.textColor = .white
+        
+        var myLocateLabel = UILabel()
+        myLocateLabel.text = "의정부시"
+        myLocateLabel.font = UIFont(name: "SFProText-Medium", size: 17)
+        myLocateLabel.textColor = .white
+        
+        var myweatherLabel = UILabel()
+        myweatherLabel.text = "흐림"
+        myweatherLabel.font = UIFont(name: "SFProText-Medium", size: 16)
+        myweatherLabel.textColor = .white
+        
+        var currentTemperature = UILabel()
+        currentTemperature.text = "21°"
+        currentTemperature.font = UIFont(name: "SFProText-Light", size: 52)
+        currentTemperature.textColor = .white
+        
+        var todayHighTemperatureLabel = UILabel()
+        todayHighTemperatureLabel.text = "최고:29°"
+        todayHighTemperatureLabel.font = UIFont(name: "SFProText-Medium", size: 15)
+        todayHighTemperatureLabel.textColor = .white
+        
+        var todayLowTemperatureLabel = UILabel()
+        todayLowTemperatureLabel.text = "최저:15°"
+        todayLowTemperatureLabel.font = UIFont(name: "SFProText-Medium", size: 15)
+        todayLowTemperatureLabel.textColor = .white
+
+        [myLocationLabel, myLocateLabel, myweatherLabel, currentTemperature, todayHighTemperatureLabel, todayLowTemperatureLabel].forEach{
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            weatherListImageView.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([myLocationLabel.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 10),
+                                     myLocationLabel.leadingAnchor.constraint(equalTo: weatherListImageView.leadingAnchor, constant: 16)])
+        NSLayoutConstraint.activate([myLocateLabel.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 44),
+                                     myLocateLabel.leadingAnchor.constraint(equalTo: weatherListImageView.leadingAnchor, constant: 16)])
+        NSLayoutConstraint.activate([myweatherLabel.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 87),
+                                     myweatherLabel.leadingAnchor.constraint(equalTo: weatherListImageView.leadingAnchor, constant: 16)])
+        NSLayoutConstraint.activate([currentTemperature.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 4),
+                                     currentTemperature.leadingAnchor.constraint(equalTo: weatherListImageView.leadingAnchor, constant: 249)])
+        NSLayoutConstraint.activate([todayHighTemperatureLabel.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 87),
+                                     todayHighTemperatureLabel.leadingAnchor.constraint(equalTo: myweatherLabel.leadingAnchor, constant: 180)])
+        NSLayoutConstraint.activate([todayLowTemperatureLabel.topAnchor.constraint(equalTo: weatherListImageView.topAnchor, constant: 87),
+                                     todayLowTemperatureLabel.leadingAnchor.constraint(equalTo: todayHighTemperatureLabel.leadingAnchor, constant: 66)])
+        
+        return cardView
     }()
     
     override func viewDidLoad() {
@@ -97,6 +154,12 @@ final class ViewController: UIViewController {
                                      searchTextField.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20),
                                      searchTextField.widthAnchor.constraint(equalToConstant: 335),
                                      searchTextField.heightAnchor.constraint(equalToConstant: 40)])
+        
+        weatherCardView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(weatherCardView)
+        
+        NSLayoutConstraint.activate([weatherCardView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 204),
+                                     weatherCardView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20)])
     }
 }
 
