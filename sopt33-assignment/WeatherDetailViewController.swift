3adfc9f6390384add4775bec0867b24fede0af31
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-10-23 17:07:54 +0900

    [Feat] #1 디테일뷰 기본 레이아웃 구현

--- sopt33-assignment/WeatherDetailViewController.swift
+++ sopt33-assignment/WeatherDetailViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 final class WeatherDetailViewController: UIViewController{
+    
     private var backgroundImageView: UIImageView = {
         let backgroundImage = UIImage(named: "background")
         let backgroundImageView = UIImageView(image: backgroundImage)
+        backgroundImageView.contentMode = .scaleAspectFill
         return backgroundImageView
     }()
     
@@ -81,12 +83,63 @@ final class WeatherDetailViewController: UIViewController{
         label.lineBreakMode = .byWordWrapping
         var paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.02
-        label.attributedText = NSMutableAttributedString(string: "08:00~09:00에 강우 상태가, 18:00에 한때 흐린 상태가 예상됩니다.", 
+        label.attributedText = NSMutableAttributedString(string: "08:00~09:00에 강우 상태가, 18:00에 한때 흐린 상태가 예상됩니다.",
                                                          attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
-        
         return label
     }()
     
+    private var navigationLine: UIView = {
+        var view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 375, height: 0)
+        view.alpha = 0.3
+        
+        var shadows = UIView()
+        shadows.frame = view.frame
+        shadows.clipsToBounds = false
+        view.addSubview(shadows)
+        
+        let shadowPath0 = UIBezierPath(roundedRect: shadows.bounds, cornerRadius: 0)
+        
+        let layer0 = CALayer()
+        layer0.shadowPath = shadowPath0.cgPath
+        layer0.shadowColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4).cgColor
+        layer0.shadowOpacity = 1
+        layer0.shadowRadius = 2
+        layer0.shadowOffset = CGSize(width: 0, height: 0)
+        layer0.bounds = shadows.bounds
+        layer0.position = shadows.center
+        shadows.layer.addSublayer(layer0)
+        
+        var stroke = UIView()
+        stroke.bounds = view.bounds.insetBy(dx: -0.2, dy: -0.2)
+        stroke.center = view.center
+        view.addSubview(stroke)
+        view.bounds = view.bounds.insetBy(dx: -0.2, dy: -0.2)
+        stroke.layer.borderWidth = 0.4
+        stroke.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        
+        return view
+    }()
+    
+    private var mapImageView: UIImageView = {
+       let imageView = UIImageView(image: UIImage(named: "map"))
+        return imageView
+    }()
+    
+    private var listImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "list"))
+        return imageView
+    }()
+    
+    private var pointerImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "pointer"))
+        return imageView
+    }()
+    
+    private var dotImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "dot"))
+        return imageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -126,7 +179,9 @@ final class WeatherDetailViewController: UIViewController{
         }
         
         NSLayoutConstraint.activate([roundedRectangle.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 334),
-                                     roundedRectangle.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 20)])
+                                     roundedRectangle.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 20),
+                                     roundedRectangle.widthAnchor.constraint(equalToConstant: 335),
+                                     roundedRectangle.heightAnchor.constraint(equalToConstant: 212)])
         NSLayoutConstraint.activate([lineInRectangle.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 400),
                                      lineInRectangle.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 34)])
         
@@ -136,6 +191,25 @@ final class WeatherDetailViewController: UIViewController{
         }
         
         NSLayoutConstraint.activate([descriptionLabel.topAnchor.constraint(equalTo: roundedRectangle.topAnchor, constant: 10),
-                                     descriptionLabel.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 35)])
+                                     descriptionLabel.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 35),
+                                     descriptionLabel.widthAnchor.constraint(equalToConstant: 305),
+                                     descriptionLabel.heightAnchor.constraint(equalToConstant: 45)])
+        
+        [navigationLine, mapImageView, listImageView, pointerImageView, dotImageView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            backgroundImageView.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([navigationLine.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 730),
+                                     navigationLine.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor),
+                                     navigationLine.widthAnchor.constraint(equalToConstant: 375)])
+        NSLayoutConstraint.activate([mapImageView.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 734),
+                                     mapImageView.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 10)])
+        NSLayoutConstraint.activate([listImageView.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 734),
+                                     listImageView.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 322)])
+        NSLayoutConstraint.activate([pointerImageView.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 734),
+                                     pointerImageView.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 161)])
+        NSLayoutConstraint.activate([dotImageView.topAnchor.constraint(equalTo: backgroundImageView.topAnchor, constant: 734),
+                                     dotImageView.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 189)])
     }
 }
